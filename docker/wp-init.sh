674c3922cbfa2f071c2e9f17fe34793acca533c3
willopez <will@noomadigital.com>
2017-11-10 22:55:43 -0800

    Add plugins to make WordPress more like a CMS that a blogging platform.

--- docker/wp-init.sh
+++ docker/wp-init.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# Perform WordPress installation
 docker exec admin.example.local wp --path=/usr/src/wordpress db reset --yes
 docker exec admin.example.local wp --path=/usr/src/wordpress core install --url=http://admin.example.local --title="Example Admin" --admin_user=admin --admin_password=admin --admin_email=example@example.com
+
+# Install plugins and themes
 docker exec admin.example.local wp --path=/usr/src/wordpress plugin install gutenberg --activate
+docker exec admin.example.local wp --path=/usr/src/wordpress plugin install disable-blogging --activate
+docker exec admin.example.local wp --path=/usr/src/wordpress plugin install https://github.com/willopez/fancy-admin-ui/archive/master.zip --activate --force
 docker exec admin.example.local wp --path=/usr/src/wordpress theme install twentyseventeen --activate
-docker exec admin.example.local wp --path=/usr/src/wordpress plugin update --all
+
+# Delete unnecessary plugins and themes
+docker exec admin.example.local wp --path=/usr/src/wordpress plugin delete hello
+docker exec admin.example.local wp --path=/usr/src/wordpress plugin delete akismet
+docker exec admin.example.local wp --path=/usr/src/wordpress theme delete twentyfifteen
+docker exec admin.example.local wp --path=/usr/src/wordpress theme delete twentysixteen
+
+# Set url structure
 docker exec admin.example.local wp --path=/usr/src/wordpress rewrite structure '/%postname%/' --hard
-docker exec admin.example.local wp --path=/usr/src/wordpress core update
