333a45a30d1d7709d717ac18f2dc31d3e79fc9b6
willopez <will@noomadigital.com>
2017-11-16 15:13:27 -0800

    Consolidate initialization WP-CLI commands

--- wordpress/wp-init.sh
+++ wordpress/wp-init.sh
@@ -5,18 +5,15 @@ docker exec admin.example.local wp --path=/usr/src/wordpress db reset --yes
 docker exec admin.example.local wp --path=/usr/src/wordpress core install --url=http://admin.example.local --title="Example Admin" --admin_user=admin --admin_password=admin --admin_email=example@example.com
 
 # Install plugins and themes
-docker exec admin.example.local wp --path=/usr/src/wordpress plugin install gutenberg --activate
-docker exec admin.example.local wp --path=/usr/src/wordpress plugin install wp-stateless --activate
-docker exec admin.example.local wp --path=/usr/src/wordpress plugin install disable-blogging --activate
-docker exec admin.example.local wp --path=/usr/src/wordpress plugin install white-label-cms --activate
-docker exec admin.example.local wp --path=/usr/src/wordpress plugin install https://github.com/willopez/fancy-admin-ui/archive/master.zip --activate --force
+docker exec admin.example.local wp --path=/usr/src/wordpress plugin install gutenberg wp-stateless disable-blogging white-label-cms https://github.com/willopez/fancy-admin-ui/archive/master.zip --activate
 docker exec admin.example.local wp --path=/usr/src/wordpress theme install twentyseventeen --activate
 
 # Delete unnecessary plugins and themes
-docker exec admin.example.local wp --path=/usr/src/wordpress plugin delete hello
-docker exec admin.example.local wp --path=/usr/src/wordpress plugin delete akismet
-docker exec admin.example.local wp --path=/usr/src/wordpress theme delete twentyfifteen
-docker exec admin.example.local wp --path=/usr/src/wordpress theme delete twentysixteen
+docker exec admin.example.local wp --path=/usr/src/wordpress plugin delete hello akismet
+docker exec admin.example.local wp --path=/usr/src/wordpress theme delete twentyfifteen twentysixteen
 
 # Set url structure
 docker exec admin.example.local wp --path=/usr/src/wordpress rewrite structure '/%postname%/' --hard
+
+# Fix permissions
+docker exec admin.example.local chown -R nobody.nobody /var/www/wp-content
