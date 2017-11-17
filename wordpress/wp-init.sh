db7076012f789e7310133e7e36e42b7c45191d3f
willopez <will@noomadigital.com>
2017-11-16 17:01:08 -0800

    Create default pages programatically

--- wordpress/wp-init.sh
+++ wordpress/wp-init.sh
@@ -4,8 +4,12 @@
 docker exec admin.example.local wp --path=/usr/src/wordpress db reset --yes
 docker exec admin.example.local wp --path=/usr/src/wordpress core install --url=http://admin.example.local --title="Example Admin" --admin_user=admin --admin_password=admin --admin_email=example@example.com
 
+# Create default pages
+docker exec admin.example.local wp --path=/usr/src/wordpress post create --post_type=page --post_title='Home' --post_name='home' --post_content='Welcome to NoomaPress' --post_status='publish'
+docker exec admin.example.local wp --path=/usr/src/wordpress post create --post_type=page --post_title='About' --post_name='about' --post_content='About...' --post_status='publish'
+
 # Install plugins and themes
-docker exec admin.example.local wp --path=/usr/src/wordpress plugin install gutenberg wp-stateless disable-blogging white-label-cms redis-cache https://github.com/willopez/fancy-admin-ui/archive/master.zip --activate
+docker exec admin.example.local wp --path=/usr/src/wordpress plugin install gutenberg wp-stateless disable-blogging white-label-cms redis-cache https://github.com/willopez/fancy-admin-ui/archive/master.zip --activate --force
 docker exec admin.example.local wp --path=/usr/src/wordpress theme install twentyseventeen --activate
 
 # Delete unnecessary plugins and themes
