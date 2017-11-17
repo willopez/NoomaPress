db9cb54986a5960988a2afdc83999442895b57d6
willopez <will@noomadigital.com>
2017-11-16 16:44:18 -0800

    Add redis-cache plugin

--- wordpress/wp-init.sh
+++ wordpress/wp-init.sh
@@ -5,7 +5,7 @@ docker exec admin.example.local wp --path=/usr/src/wordpress db reset --yes
 docker exec admin.example.local wp --path=/usr/src/wordpress core install --url=http://admin.example.local --title="Example Admin" --admin_user=admin --admin_password=admin --admin_email=example@example.com
 
 # Install plugins and themes
-docker exec admin.example.local wp --path=/usr/src/wordpress plugin install gutenberg wp-stateless disable-blogging white-label-cms https://github.com/willopez/fancy-admin-ui/archive/master.zip --activate
+docker exec admin.example.local wp --path=/usr/src/wordpress plugin install gutenberg wp-stateless disable-blogging white-label-cms redis-cache https://github.com/willopez/fancy-admin-ui/archive/master.zip --activate
 docker exec admin.example.local wp --path=/usr/src/wordpress theme install twentyseventeen --activate
 
 # Delete unnecessary plugins and themes
